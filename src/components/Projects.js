77b5221d8742c4af1f9420167a2c1f8a4638806a
Charles Daoust <daoustcharles@yahoo.com>
2019-12-21 04:04:25 -0500

    Changed a few things

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,48 @@ import store from './pics/cupcake_website.png'
 
 function Project(){
     return(
+    <div className ="container">
      <div className="gallery">
-         <a href='https://github.com/' class='big'><img  src={restaurant} alt="restaurant"/></a>
-         <a href='https://github.com/' class='big'><img  src={store} alt="store"/></a>  
+         <div class="box">
+              <img id='pic' src={restaurant} alt="restaurant"/>
+              <div class ="text"> 
+              <h2> Restaurant website</h2>
+              <p> Using Django I created a brochure style 
+                  website that dynamically displays menu 
+                  contents stored in an SQLite database.
+                   Visitors can browse the menus on different 
+                   screen sizes and if they have questions they 
+                   can send an email through the email form on the
+                    contact page
+                    </p>
+              <a href="https://github.com/madpineapple/django_restaurant">
+              Click here to view  Github repository</a>
+                 
+              </div>
+         </div>
+
+         <div class="box">
+        <img id='pic' src={store} alt="store"/> 
+         <div class ="text"> 
+              <h2> Simply Cupcakes(E-commerce) Website</h2>
+              <p> Using Node.js, Express, and MySQL 
+                  this ecommerce website allows visitors 
+                  to view the various types of cupcakes
+                   Simply Cupcakes has to offer.
+                   Visitors can register and login in to 
+                   their customer accounts. They can also add 
+                   it to their cart and proceed to checkout. 
+                   Once at checkout the visitor will confirm 
+                   their address and then will complete the 
+                   transaction using Stripe checkout. 
+
+                </p>
+                <a href="https://github.com/madpineapple/js_store">
+                    Click here to view  Github repository</a>
+              </div>
+         </div>
+         
+     </div>
      </div>
     )
 }
